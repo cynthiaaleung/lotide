5f8ec3da971174dfeb4f55ffb9938fbb217e9a51
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-09-14 16:50:27 -0700

    assertArraysEqual created

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
