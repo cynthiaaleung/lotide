db069b35b0016ceee4a2c58991b0ff6f893edf4d
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-09-14 16:17:04 -0700

    Comparing arrays

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([], []), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], []), false); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
