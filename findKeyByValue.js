63875f99f125928024b6517ac28502af51378843
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-09-15 15:09:38 -0700

    created findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  const keysArray = Object.keys(object);
+  for (let key of keysArray) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  romantic: "The Proposal",
+  romanceTheme: "The Proposal"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Proposal"), "romantic");
