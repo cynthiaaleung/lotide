1003a5fc1f087fa0c2b3ad74c9eb7c58dcf218cc
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-09-15 13:11:04 -0700

    created countLetters function

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const results = {};
+
+  for (let letter of string) {
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+console.log(countLetters("lighthouse in the house"));
+console.log(countLetters("LHL"));
+console.log(countLetters("Bootcamp"));
