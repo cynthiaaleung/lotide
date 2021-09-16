3df2c4eeda9159509dfb2d13b868e84bce669c7b
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-09-15 22:12:46 -0700

    modified

--- middle.js
+++ middle.js
@@ -2,15 +2,13 @@ const eqArrays = function(firstArray, secondArray) {
   if (firstArray.length !== secondArray.length) {
     return false;
   }
-
   for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) {
+    if (!secondArray.includes(firstArray[i])) {
       return false;
     }
   }
   return true;
 };
-
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
