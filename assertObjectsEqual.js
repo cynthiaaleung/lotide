afe9c03153f292298295818c0632579f55ed1840
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-09-19 14:17:14 -0700

    added comments

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -35,8 +35,9 @@ const eqObjects = function(object1, object2) {
 
 
 const assertObjectsEqual = function(actual, expected) {
-  const inspect = require('util').inspect;
-  
+  const inspect = require('util').inspect; //import util library's inspect func so we can use it
+  // console.log(`Example label: ${inspect(actual)}`) : this is an example of how to use inspect
+
   if (eqObjects(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
@@ -44,6 +45,6 @@ const assertObjectsEqual = function(actual, expected) {
   }
 };
 
-console.log(assertObjectsEqual({ a: "1", b: "2" }, { b: "2", a: "1" }));
-console.log(assertObjectsEqual({ a: "1", b: "2" }, { b: "2", a: "2" }));
-console.log(assertObjectsEqual({ a: "1", b: "2" }, { b: "1", a: "2", c: "3" }));
+assertObjectsEqual({ a: "1", b: "2" }, { b: "2", a: "1" });
+assertObjectsEqual({ a: "1", b: "2" }, { b: "2", a: "2" });
+assertObjectsEqual({ a: "1", b: "2" }, { b: "1", a: "2", c: "3" });
