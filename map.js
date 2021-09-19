e3c15c68e3b605983fdade88700f8093c9605b09
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-09-19 13:58:32 -0700

    edited

--- map.js
+++ map.js
@@ -25,14 +25,14 @@ const assertArraysEqual = function(actual, expected) {
 const words = ["ground", "control", "to", "major", "tom"];
 
 const map = function(array, callback) {
-  const results = [];
+  let results = [];
   for (let item of array) {
     results.push(callback(item));
   }
   return results;
 };
 
-const results1 = map(words, word => word[0]);
+let results1 = map(words, word => word[0]);
 console.log(results1);
 
 // test cases
