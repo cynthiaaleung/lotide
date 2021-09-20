08c574257241dbe267c95502f905ae5535579ff7
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-09-20 13:37:51 -0700

    modified

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -27,3 +27,5 @@ const bestTVShowsByGenre = {
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Proposal"), "romantic");
+
+module.exports = findKeyByValue;
