102ed8bde8fe2345adaa0cf9f125c818a0fa98d8
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-09-20 13:38:47 -0700

    modified

--- without.js
+++ without.js
@@ -70,3 +70,5 @@ assertArraysEqual(withoutAlternateSolution([], [1, 2]), []);
 withoutAlternateSolution(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+module.exports = without;
