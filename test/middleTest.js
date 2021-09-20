90c13e47e0579185b77754d613568ef41af4068a
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-09-20 12:53:39 -0700

    modified wth Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,22 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+describe("#middle", () => {
+
+  it("returns [] for middle([])", () => {
+    assert.deepEqual(middle([]), []);
+  });
+  it("returns [] for middle([1])", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns [] for middle([1, 2])", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("returns [2] for middle([1, 2, 3])", () => {
+    assert.deepEqual(middle([]), []);
+  });
+  it("returns [2, 3] for middle([1, 2, 3, 4])", () => {
+    assert.deepEqual(middle([]), []);
+  });
+});
+
