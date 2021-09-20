9faaba53d9c5b2401a1f7dce20c0ec4f5d92dfcd
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-09-20 13:38:16 -0700

    modified

--- takeUntil.js
+++ takeUntil.js
@@ -60,3 +60,5 @@ assertArraysEqual(takeUntil(data1, x => x < 0),[ 1, 2, 5, 7, 2 ]);
 assertArraysEqual(takeUntil(data2, x => x === ','), [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
 assertArraysEqual(takeUntil(data3, x => x === 1),[]);
 assertArraysEqual(takeUntil(data4, x => x > 9),[ 1, 2, 5, 7, 8 ]);
+
+module.exports = takeUntil;
